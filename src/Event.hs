6e7026a5246db240060b2aa583f2924546841a5b
Mike Limansky <mikhael.limanskii@teleca.com>
2011-09-24 03:18:31 +0400

    Add Event to iCalendar converter funciton.

--- src/Event.hs
+++ src/Event.hs
@@ -1,6 +1,7 @@
 module Event where
 
 import Data.Time
+import System.Locale
 
 data Duration = Interval NominalDiffTime | AllDay deriving (Eq)
 
@@ -23,3 +24,29 @@ instance Show Event where
               period = case eventDuration e of
                  AllDay     -> show . utctDay $ s
                  Interval i -> show s ++ " - " ++ (show $ addUTCTime i s)
+
+calBegin = "BEGIN"
+calEnd = "END"
+calVevent = "VEVENT"
+calDtStart = "DTSTART"
+calDtEnd = "DTEND"
+calSummary = "SUMMARY"
+calDescription = "DESCRIPTION"
+isoTimeFormat = "%Y%m%dT%H%M%SZ"
+crlf = "\r\n"
+
+calLine key value = key ++ ": " ++ value ++ crlf
+
+eventToICal e = calLine calBegin calVevent
+    ++ calLine calDtStart (formatTime defaultTimeLocale isoTimeFormat $ eventStart e)
+    ++ endTimeLine
+    ++ calLine calSummary (eventTitle e)
+    ++ calLine calDescription (head desc)
+    ++ descFooter
+    ++ calLine calEnd calVevent
+        where endTimeLine = case eventDuration e of
+                AllDay -> ""
+                Interval i -> let endTime = addUTCTime i (eventStart e)
+                              in calLine calDtEnd (formatTime defaultTimeLocale isoTimeFormat endTime)
+              desc = lines . eventDescription $ e
+              descFooter = concat $ map (\s -> " " ++ s ++ crlf) (tail desc)
