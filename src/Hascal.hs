2fc21445cec9621ccf277e728cac9404e7328ba3
Mike Limansky <mikhael.limanskii@teleca.com>
2011-09-25 18:11:12 +0400

    Support import ics file from command line.
    Just start parsing and show parse result.

--- src/Hascal.hs
+++ src/Hascal.hs
@@ -1,5 +1,31 @@
 -- main module
 
 import Event
+import System.Console.GetOpt
+import System.Environment (getArgs)
+import System.IO
 
-main = putStrLn "Welcome to Hascal world :)"
+data Flag = Import String deriving Show
+
+options = 
+    [ Option ['i']      ["import"]      ( ReqArg Import "FILE" )   "file to be imported"
+    ]
+
+main = do
+    putStrLn "Welcome to Hascal world :)"
+    args <- getArgs
+    case getOpt RequireOrder options args of 
+        (flags, [], []) -> mapM doOpt flags
+        (_, _, msgs)    -> error $ concat msgs ++ usage
+        where usage = usageInfo usageHeader options
+
+
+usageHeader = "Usage: hascal [OPTIONS]:"
+
+doOpt (Import fn) = importICS fn
+
+importICS fn = do
+    putStrLn $ "Importing file:" ++ fn
+    h <- openFile fn ReadMode
+    cont <- hGetContents h
+    putStrLn . show $ icalToEvent cont
