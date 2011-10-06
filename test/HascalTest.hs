eb0c261fc83504ca17957847281ba4678f5337cf
Victor Portnov <vic.portnov@gmail.com>
2011-10-06 18:25:31 +0400

    Add escaped and empty tests for uid parser
    1. Some general HUnit support is added
    2. Escaped test is added for uid parser
    3. Empty test is added for uid parser

--- test/HascalTest.hs
+++ test/HascalTest.hs
@@ -1,11 +1,16 @@
 import Test.Framework (defaultMain, testGroup)
 import Test.Framework.Providers.QuickCheck2 (testProperty)
+import Test.Framework.Providers.HUnit (testCase)
 import ICalParserTests
 
 main = defaultMain tests
 
 tests = [
         testGroup "Parser tests" [
-                testProperty "Random test for uid" prop_uid
+                testGroup "Uid parser tests" [
+                      testProperty "Random" prop_uid
+                    , testCase "Escaped values" test_uid_escaped_value
+                    , testCase "Empty value" test_uid_empty
+                ]
             ]
         ]
--- test/ICalParserTests.hs
+++ test/ICalParserTests.hs
@@ -8,6 +8,7 @@ import ICalDefs
 import Data.Char (isPunctuation)
 import Text.ParserCombinators.Parsec (parse)
 import Test.QuickCheck
+import Test.HUnit
 
 (===) :: Eq a => Either t a -> a -> Bool
 (Right a) === b = a == b
@@ -34,9 +35,22 @@ isNonEscapedText xs = all isTextChar xs && isNonEscaped xs
 
 isEscapedChar a = a == '\\' || a == ';' || a == ',' || a == 'N' || a == 'n'
 
-prop_uid (NonEscapedNonEmptyText s) = parse uid "" ("UID:" ++ escape s ++ "\r\n") === Uid s
-
 escape [] = []
 escape (x:xs)
     | isEscapedChar x && isPunctuation x = '\\' : x : escape xs
     | otherwise = x : escape xs
+
+(@?==) :: (Show a, Show a1, Eq a) => Either a1 a -> a -> Assertion
+(Right a) @?== b = a == b @? "expected: " ++ (show b) ++ "\n but got: " ++ (show a) ++ "\n"
+(Left err) @?== _ = assertFailure $ show err
+
+(@?!) :: Either t t1 -> [Char] -> Assertion
+(Left _) @?! _ = return ()
+_ @?! s = assertFailure $ "fail expected but passed\n Message: " ++ s
+
+-- uid parser tests
+prop_uid (NonEscapedNonEmptyText s) = parse uid "" ("UID:" ++ escape s ++ "\r\n") === Uid s
+
+test_uid_escaped_value = parse uid "" "UID:1: v1\\, v2\\;\\n2: v3\\N3:\r\n" @?== Uid "1: v1, v2;\n2: v3\n3:"
+
+test_uid_empty = parse uid "" "UID:\r\n" @?! "UID can't be empty"
